37aca743ca37352683f9a6e6c4327cec5f2eeda9
Anastasiya Gromova <anastasiya.gromova.contractor@pepsico.com>
2024-03-27 08:54:00 -0700

    Added search flow implementation using langchain

--- flows/search_flow_langchain/flow.py
+++ flows/search_flow_langchain/flow.py
@@ -1,4 +1,6 @@
 """Search Flow implementation in LangChain."""
+import uuid
+import requests
 from langchain_openai import AzureChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from mlops.common.config_utils import MLOpsConfig
@@ -8,48 +10,68 @@ from langchain.callbacks.manager import Callbacks
 from langchain.schema.output_parser import StrOutputParser
 from langchain_community.utilities import BingSearchAPIWrapper
 from langchain_community.tools.bing_search import BingSearchResults
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
+
+@dataclass
+class SummaryResult:
+    content: str
 
 
 @tool
-def bing_search_tool(url: str, callbacks: Callbacks = None):
+def bing_search_tool(question: str, callbacks: Callbacks = None):
     """Search using the Azure Bing Search API."""
     config = MLOpsConfig()
     bing_wrapper = BingSearchAPIWrapper()
-    chain = (
-        ChatPromptTemplate.from_template("Search for relevant information based on the following URL:\n{{uid}}>")
-        .partial(url=url)
-        | AzureChatOpenAI(
-            api_version=config.gpt35_turbo_config["aoai_api_version"],
-            azure_deployment=config.gpt35_turbo_config["aoai_deployment_name"]
-        )
-        | StrOutputParser()
-        | BingSearchResults(
-            name="bing_search_results",
-            api_wrapper=bing_wrapper,
-            url=url
-        )
-    ).with_config({"run_name": generate_run_name()})
+    search_results = bing_wrapper.results(question, 5) # Run query through BingSearch and return snippet, title, and link metadata
+    return search_results
 
-    return chain.invoke(
-        {"url": url},
-        {"callbacks": callbacks},
-    )
+
+@tool
+def summarize_tool(url: str, callbacks: Callbacks = None):
+    """Summarize a website."""
+    config = MLOpsConfig()
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, headers=headers)
+    if response.status_code==200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        text = ' '.join([p.text for p in soup.find_all('p')])
+
+        summary_chain = (
+            ChatPromptTemplate.from_template(
+                "Summarize the following text:\n<TEXT {uid}>\n" "{text}" "\n</TEXT {uid}>"
+            ).partial(uid=lambda: uuid.uuid4())
+            | AzureChatOpenAI(
+                api_version=config.gpt35_turbo_config["aoai_api_version"],
+                azure_deployment=config.gpt35_turbo_config["aoai_deployment_name"]
+            )
+            | StrOutputParser()
+        ).with_config(run_name="Summarize Text")
+
+        summary_text = summary_chain.invoke(
+            {"text": text},
+            {"callbacks": callbacks},
+        )
+        return SummaryResult(content=summary_text)
+    else:
+        # If request was not successful, return an error message
+        return SummaryResult(content="Failed to retrieve content from the URL.")
 
 
 def process(input: dict):
     """Implement search flow using LangChain.
 
     Args:
-        input (dict): Input dictionary containing the question and URL.
+        input (dict): Input dictionary containing the question.
 
     Returns:
         dict: A dictionary containing the search result.
     """
     try:
-        search_result = bing_search_tool(input["url"])
-
-        # print("Search Result:", search_result)
-        return {"summary": search_result}
+        results  = bing_search_tool(input["question"])
+        top_link = results[0]['link']
+        result = summarize_tool(top_link)
+        return {"answer": result}
     except Exception as e:
         print("Error occurred during search:", e)
-        return {"summary": "Search failed due to an error."}
+        return {"answer": "Search failed due to an error."}
--- flows/search_flow_langchain/run_flow.py
+++ flows/search_flow_langchain/run_flow.py
@@ -9,10 +9,9 @@ PROJECT_NAME = "Search Flow LangChain"
 def main():
     """Implement search flow using LangChain."""
     os.environ["LANGCHAIN_PROJECT"] = PROJECT_NAME
-    url = ("https://learn.microsoft.com/en-us/azure/machine-learning/"
-           "prompt-flow/how-to-integrate-with-langchain?view=azureml-api-2")
+    question = "What is Microsoft's mission?"
 
-    message = {"url": url}
+    message = {"question": question}
     process(message)
 
 
